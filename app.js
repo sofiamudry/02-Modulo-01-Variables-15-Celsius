3eec16b3cb3cc9be5a8467185a0570eeb6acd38e
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 15:41:11 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Celsius');
+
+alert('Celsius a Fahrenheit');
+
+const celsius = prompt ('Ingrese los grados en Celsius');
+const fahrenheit = (celsius*(9/5)+32);
+
+alert(`Son ${fahrenheit}ºF`);
